329fa1d3062600dce42bbf72ade359df226abafe
vincent <vincent@hyvertvincent.electrolight.org>
2018-11-25 19:25:05 +0100

    Update

--- src/main/java/me/vinceh121/gttts/GoogleTranslateTTS.java
+++ src/main/java/me/vinceh121/gttts/GoogleTranslateTTS.java
@@ -1,14 +1,10 @@
 package me.vinceh121.gttts;
 
-import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.PipedInputStream;
-import java.io.PipedOutputStream;
 import java.net.URL;
 import java.net.URLEncoder;
 
@@ -29,6 +25,7 @@ public class GoogleTranslateTTS {
 		byte[] data = getData(text);
 		FileOutputStream out = new FileOutputStream(file);
 		out.write(data);
+		out.close();
 	}
 
 	public byte[] getData(String text) throws IOException {
